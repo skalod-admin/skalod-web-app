68e7a109666d9d2191513b7b0faf76b809b11ff5
Manthan Bhosle <63637662+manthanBhosle@users.noreply.github.com>
2023-09-27 12:11:02 +0000

    feat(header): transition styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,36 @@
+const classnames = require('classnames');
+const throttle = require('lodash/throttle');
+import { useEffect, useState } from 'react';
 import logo from '../utils/logo.jpeg'
 
 const Header = () => {
-
+  const [isActive, setIsActive] = useState(false);
+  handleScroll = () => {
+    if(window.scrollY > 0) {
+      setIsActive(true);
+    }else {
+      setIsActive(false);
+    }
+  }
   const scrollToSection = (sectionId) => {
     document.getElementById(sectionId).scrollIntoView({
       behavior: 'smooth'
     });
   };
 
+  useEffect(() => {
+    const throttledScroll = throttle(handleScroll, 100);
+    window.addEventListener('scroll', throttledScroll);
+
+    return () => {
+      window.removeEventListener('scroll', throttledScroll);
+    };
+  });
+
   return(
-    <div className="header">
+    <div className={classnames("header", {
+      "active": isActive
+    })}>
       <div className="logo-container">
         <img className="logo" src={logo} />
         <h1>SKALOD</h1>
